19991c81b00d76d1c208ef3f93a3378f3fc3a471
Cristhian Balta <cristhianbalta@gmail.com>
2026-08-08 22:44:05 -0500

    Update retriever test for REST API

--- tests/test_retriever.py
+++ tests/test_retriever.py
@@ -1,14 +1,14 @@
-from app.retriever import retrieve_documents, vector_store
+from app.retriever import retrieve_documents
+
 
 def test_retriever_returns_documents():
-
     question = "What are the company's climate goals?"
 
     docs = retrieve_documents(question, k=3)
 
+    assert len(docs) > 0
 
     for i, doc in enumerate(docs):
-
         print("\n================")
         print(f"Result {i+1}")
         print(doc.page_content)
@@ -17,8 +17,5 @@ def test_retriever_returns_documents():
         print(doc.metadata)
 
 
-    vector_store.client.close()
-    print("\nQdrant closed successfully!")
-    
 if __name__ == "__main__":
     test_retriever_returns_documents()
